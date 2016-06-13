405728daae595f75702fb845104341fec8e26837
cary.mcpoland <cary.mcpoland@biznetsolutions.com>
2016-06-13 21:27:01 +0100

    Initial commit

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+    authors:
+    [
+        {
+            id: '1',
+            firstName: 'Joe',
+            lastName: 'Bloggs'
+        },
+        {
+            id: '2',
+            firstName: 'Pablo',
+            lastName: 'Escobar'
+        },
+        {
+            id: '3',
+            firstName: 'Georgey',
+            lastName: 'Porgey'
+        }
+    ]
+};
