34cc3e11decee4dd2fd1704f3595ddb23b9ebe53
cary.mcpoland <cary.mcpoland@biznetsolutions.com>
2016-06-15 15:34:08 +0100

    Introducing React-Router to handle routing within the application

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var React = require('react');
+
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
+
+var routes = (
+    <Route name="app" path="/" handler={require('./components/app')}>
+        <DefaultRoute handler={require('./components/homePage')}/>
+        <Route name="authors" handler={require('./components/authors/authorPage')}/>
+        <Route name="about" handler={require('./components/about/aboutPage')}/>
+        <NotFoundRoute handler={require('./components/notFoundPage')}/>
+        <Redirect from="about-us" to="about" />
+    </Route>
+);
+
+module.exports = routes;
