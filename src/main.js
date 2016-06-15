c60dd262055fc7b6cbc302dabb0442290dd5bbff
cary.mcpoland <cary.mcpoland@biznetsolutions.com>
2016-06-15 18:01:28 +0100

    Changed application to use HTML5 History API (Limited support for older browsers)

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
-Router.run(routes, function(Handler) {
+Router.run(routes, Router.HistoryLocation, function(Handler) {
     React.render(<Handler/>, document.getElementById('app'));
 });
 
